39f72e577984cc8ffd1b7ad542323874182cb94b
shieldnova <shieldnova.in@gmail.com>
2025-10-08 22:17:06 +0530

    SITEMAP UPDATED

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwincss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite';
 import postcss from '@tailwindcss/postcss';
 import ViteSitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwincss(),
+    tailwindcss(),
     postcss(),
     ViteSitemap({
       hostname: 'https://www.markyet.in',
+      routes: ['/', '/about', '/services', '/blog', '/contact'],
     }),
   ],
 });
